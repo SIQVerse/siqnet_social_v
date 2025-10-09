64d3348c762a1cb73e8724fe23c19d2ce8442f1b
SIQVerse <smlzulu@gmail.com>
2025-10-09 15:26:21 -0700

    Update views.py git add userauth/views.py git commit -m "Fix login_required import in views.py" git push origin main

--- userauth/views.py
+++ userauth/views.py
@@ -1,69 +1,41 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib import messages
+from django.http import HttpResponse
+from .forms import RegistrationForm, LoginForm
 
-# Homepage view
-def home(request):
-    return render(request, 'userauth/home.html')
-
-# Login view
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('profile')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'userauth/login.html', {'form': form})
-
-# Logout view
-def logout_view(request):
-    logout(request)
-    return redirect('login')
-
-# Register view
 def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('profile')
+            return redirect("home")
     else:
-        form = UserCreationForm()
-    return render(request, 'userauth/register.html', {'form': form})
+        form = RegistrationForm()
+    return render(request, "register.html", {"form": form})
 
-# Profile view
-def profile_view(request):
-    return render(request, 'userauth/profile.html')
-from django.shortcuts import get_object_or_404, redirect
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
 
 @login_required
-def edit_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id, author=request.user)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'versnet/edit_post.html', {'form': form})
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from .models import Post
-from .forms import PostForm
-
-@login_required
-def edit_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id, author=request.user)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'versnet/edit_post.html', {'form': form})
+def home(request):
+    return render(request, "home.html")
