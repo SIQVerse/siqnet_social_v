573192de1caf80f5cb0be8851382d9517980e105
SIQVerse <smlzulu@gmail.com>
2025-10-09 20:17:23 -0700

    Update urls.py

--- siqnet_social/urls.py
+++ siqnet_social/urls.py
@@ -1,15 +1,8 @@
-# siqnet/urls.py
-
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
+from userauth import views  # Update this if your homepage view is in a different app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Make sure 'home' exists in userauth/views.py
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
