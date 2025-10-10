1604907cedede69941d6526c85541694339f1ff9
SIQVerse <smlzulu@gmail.com>
2025-10-09 20:36:57 -0700

    Update urls.py

--- siqnet_social/urls.py
+++ siqnet_social/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from userauth import views  # Update this if your homepage view is in a different app
+from userauth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Make sure 'home' exists in userauth/views.py
+    path('', views.home, name='home'),
 ]
