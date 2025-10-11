c29c2091e171d1bdb60fdc4733b1ec04068fd315
SIQVerse <smlzulu@gmail.com>
2025-10-11 09:07:45 -0700

    Update models.py

--- siqnet_social/models.py
+++ siqnet_social/models.py
@@ -2,15 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="siqnet_social_posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='siqnet_posts')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
