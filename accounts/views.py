50781dea964f7db9d44217d6d6f16f26a6c95b64
SIQVerse <smlzulu@gmail.com>
2025-10-11 08:29:59 -0700

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,4 @@
-# accounts/views.py
+from django.http import HttpResponse
 
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
-from django.shortcuts import render, redirect
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # Replace 'home' with your homepage URL name
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            login(request, form.get_user())
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
+def home(request):
+    return HttpResponse("Welcome to SIQNet Social V!")
