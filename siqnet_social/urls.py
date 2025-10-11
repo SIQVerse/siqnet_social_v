fccb525279b1a56902fbb83d8e7e5959737854f6
SIQVerse <smlzulu@gmail.com>
2025-10-11 08:06:34 -0700

    Update urls.py

--- siqnet_social/urls.py
+++ siqnet_social/urls.py
@@ -1,10 +1,7 @@
-# accounts/urls.py
-
-from django.urls import path
-from .views import register, login_view, logout_view
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
