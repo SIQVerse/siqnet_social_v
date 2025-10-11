92b39787e43125d096c7d6e4d1ee0ecaff7e66fd
SIQVerse <smlzulu@gmail.com>
2025-10-10 19:07:32 -0700

    Update models.py

--- siqnet_social/models.py
+++ siqnet_social/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="siqnet_posts"
+        related_name="posts_from_siqnet_social"
     )
     title = models.CharField(max_length=200)
     content = models.TextField()
