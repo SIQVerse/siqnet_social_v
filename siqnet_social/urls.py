b1651e95e235213f7924339844fac9bf25ef736d
SIQVerse <smlzulu@gmail.com>
2025-10-11 19:14:17 -0700

    Update urls.py

--- siqnet_social/urls.py
+++ siqnet_social/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("SIQNet Social V is live!")
 
 urlpatterns = [
+    path('', home, name='home'),  # Root URL
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
+
