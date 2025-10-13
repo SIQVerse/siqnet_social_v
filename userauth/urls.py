a19cf28c0f41a379b13815c357e33162ab9132f4
SIQVerse <smlzulu@gmail.com>
2025-10-13 15:29:42 -0700

    Update urls.py

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from .views import edit_profile, public_profile
+from .views import edit_profile, public_profile, profile
 
 urlpatterns = [
     path('edit-profile/', edit_profile, name='edit_profile'),
+    path('profile/', profile, name='profile'),
     path('profile/<str:username>/', public_profile, name='public_profile'),
 ]
