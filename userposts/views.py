c6502253a8b6b7b26a2172dded5b8bdbba539e29
SIQVerse <smlzulu@gmail.com>
2025-10-13 15:15:24 -0700

    Update views.py

--- userposts/views.py
+++ userposts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
-# Create your views here.
+def public_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    return render(request, 'userauth/public_profile.html', {'user': user})
