6d95b213b31afa0ccbe53ce8bc10084e4ac54238
SIQVerse <smlzulu@gmail.com>
2025-10-12 17:32:21 -0700

    Update admin.py

--- siqposts/admin.py
+++ siqposts/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
-from .models import Post
-
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('created_at',)
