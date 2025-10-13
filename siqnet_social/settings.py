c244c12890b68148831f913bd8ea03af3fb21c1f
SIQVerse <smlzulu@gmail.com>
2025-10-12 21:06:36 -0700

    Update settings.py

--- siqnet_social/settings.py
+++ siqnet_social/settings.py
@@ -1,7 +1,9 @@
+import os
+
 # Email configuration for Gmail SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'smlzulu@gmail.com'
-EMAIL_HOST_PASSWORD = 'eirm uafo zgwg lxcb'  # ← Gmail App Password
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
