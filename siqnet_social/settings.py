10cd27b2fa0e564e650dce3c32c9bfc275f07cf8
SIQVerse <smlzulu@gmail.com>
2025-10-13 04:31:25 -0700

    Update settings.py

--- siqnet_social/settings.py
+++ siqnet_social/settings.py
@@ -1,5 +1,20 @@
 INSTALLED_APPS = [
-    ...
+    # Django core apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',  # Required for django-allauth
+
+    # Your custom apps
+    'accounts',
+    'siqposts',
+    'userposts',
+    'versnet',
+
+    # Add-ons and integrations
     'rest_framework',
     'crispy_forms',
     'django_extensions',
