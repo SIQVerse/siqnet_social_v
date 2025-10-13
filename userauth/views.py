aaf42f92de7141596e7a7d36138eabe3af57a61b
SIQVerse <smlzulu@gmail.com>
2025-10-13 15:27:44 -0700

    Update views.py

--- userauth/views.py
+++ userauth/views.py
@@ -1,24 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required
-def edit_profile(request):
+def profile(request):
     user = request.user
-    profile = user.profile
+    campaigns = user.profile.campaigns.order_by('-impact_score')  # or use '-date_joined'
 
-    if request.method == 'POST':
-        # Update bio
-        profile.bio = request.POST.get('bio', profile.bio)
-
-        # Update avatar if uploaded
-        if 'avatar' in request.FILES:
-            profile.avatar = request.FILES['avatar']
-
-        # Update banner if uploaded
-        if 'banner' in request.FILES:
-            profile.banner = request.FILES['banner']
-
-        profile.save()
-        return redirect('profile')  # Redirect to the profile view
-
-    return render(request, 'userauth/edit_profile.html', {'user': user})
+    return render(request, 'userauth/profile.html', {
+        'user': user,
+        'campaigns': campaigns
+    })
