29df67bc666a0360aa82387529623e8c19129cb1
SIQVerse <smlzulu@gmail.com>
2025-10-13 19:35:46 -0700

    Update admin.py

--- siqposts/admin.py
+++ siqposts/admin.py
@@ -4,5 +4,5 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_at')
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'content', 'author__username')
     list_filter = ('created_at',)
