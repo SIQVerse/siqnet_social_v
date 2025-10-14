c319cfed62fb5472d0496613970b76b1e743e729
SIQVerse <smlzulu@gmail.com>
2025-10-13 19:59:44 -0700

    Update models.py

--- siqposts/models.py
+++ siqposts/models.py
@@ -1,21 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import User
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='siqposts')
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.title} by {self.author.username}"
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
+    class Meta:
+        unique_together = ('post', 'user')
