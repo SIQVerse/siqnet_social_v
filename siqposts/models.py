4b1d63ccc6a244d5fd16b79cde2e5997267d3e19
SIQVerse <smlzulu@gmail.com>
2025-10-13 20:20:42 -0700

    Update models.py

--- siqposts/models.py
+++ siqposts/models.py
@@ -1,6 +1,8 @@
-class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+from django.contrib.auth.models import User
 
-    class Meta:
-        unique_together = ('post', 'user')
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+
+    def __str__(self):
+        return self.user.username
