075cb4f9cb8c109d380ad5093bedd44cc27779c6
SIQVerse <smlzulu@gmail.com>
2025-10-13 20:12:18 -0700

    Update views.py

--- siqposts/views.py
+++ siqposts/views.py
@@ -1,6 +1,11 @@
-from .models import Comment
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import Post, Comment, Like
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'siqposts/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -8,8 +13,16 @@ def post_detail(request, pk):
 
     if request.method == 'POST':
         content = request.POST.get('content')
-        if content:
+        if content and request.user.is_authenticated:
             Comment.objects.create(post=post, author=request.user, content=content)
             return HttpResponseRedirect(reverse('post_detail', args=[pk]))
 
     return render(request, 'siqposts/post_detail.html', {'post': post, 'comments': comments})
+
+def toggle_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user.is_authenticated:
+        like, created = Like.objects.get_or_create(post=post, user=request.user)
+        if not created:
+            like.delete()
+    return HttpResponseRedirect(reverse('post_detail', args=[pk]))
