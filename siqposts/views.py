14bab979ff5725e56f7d6c42b6a601363400f8ff
SIQVerse <smlzulu@gmail.com>
2025-10-13 19:57:04 -0700

    Update views.py

--- siqposts/views.py
+++ siqposts/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
-
-def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'siqposts/post_list.html', {'posts': posts})
+from .models import Comment
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'siqposts/post_detail.html', {'post': post})
+    comments = post.comments.all().order_by('-created_at')
+
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:
+            Comment.objects.create(post=post, author=request.user, content=content)
+            return HttpResponseRedirect(reverse('post_detail', args=[pk]))
+
+    return render(request, 'siqposts/post_detail.html', {'post': post, 'comments': comments})
