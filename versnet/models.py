bfac0cf94e65b34da398802c72ccadf893e0c19f
SIQVerse <smlzulu@gmail.com>
2025-10-13 18:47:11 -0700

    Update models.py

--- versnet/models.py
+++ versnet/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='versnet_authored')
-    caption = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='versnet_images/')
-    posted_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='versnetposts')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.caption
+        return self.title
