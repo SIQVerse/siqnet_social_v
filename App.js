4eab9997e919947e4deea74af06bb474e334b44e
SIQVerse <smlzulu@gmail.com>
2025-10-18 15:22:57 -0700

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-export default function App() {
-  return (
-    <View><Text>Welcome to SIQNet Mobile</Text></View>
-  );
-}
