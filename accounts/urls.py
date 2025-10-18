76897ec479773ce3b02de0286e6d3b25db5dfe90
SIQVerse <smlzulu@gmail.com>
2025-10-17 19:04:03 -0700

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('password-reset/', views.password_reset, name='password_reset'),
-    path('verify-email/', views.verify_email, name='verify_email'),
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+    path('userauth/', include('userauth.urls')),
+    path('userposts/', include('userposts.urls')),
+    path('community/', include('community.urls')),
+    path('siqposts/', include('siqposts.urls')),
+    path('versnet/', include('versnet.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
